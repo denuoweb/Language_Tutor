23af9c95b6d62ab176a5cc9ec0b7949a471c3809
Jaron Rosenau <jaron.rosenau@gmail.com>
2026-05-03 02:52:36 -0700

    Use FlutterFragmentActivity for Android host

--- android/app/src/main/kotlin/com/denuoweb/language_tutor/MainActivity.kt
+++ android/app/src/main/kotlin/com/denuoweb/language_tutor/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.denuoweb.language_tutor
 
-import androidx.activity.ComponentActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val rayBanBridge = RayBanBridge()
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        rayBanBridge.register(this as ComponentActivity, flutterEngine)
+        rayBanBridge.register(this, flutterEngine)
     }
 }
